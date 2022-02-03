39fc7d8f27241cfe595fe6f7243ec4c42d063b43
Luka Cehovin Zajc <luka.cehovin@gmail.com>
2022-02-03 16:57:14 +0100

    Initial commit

---
+++ src/calibration.cpp
@@ -0,0 +1,219 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+#include <iostream>
+#include <fstream>
+#include <sys/select.h>
+#include <openservo.h>
+#include <vector>
+#include <algorithm>
+#include <yaml-cpp/yaml.h>
+
+using namespace std;
+using namespace openservo;
+
+float hardcoded_factors[] = {0.0009380807, 0.0009349769, 0.0009304994, 0.0008597145, 0.0008597145, 0.0008597145};
+
+typedef struct motor_calibration {
+	int min;
+	int max;
+	int center;
+	float factor;
+	int addr;
+	int id;
+	int position;
+	int delta;
+    vector<int> measurements;
+} motor_calibration;
+
+bool compare (const motor_calibration& i, const motor_calibration& j) { return (i.id < j.id); }
+
+vector<motor_calibration> motors;
+
+int wait_key() {
+    fd_set readSet;
+    FD_ZERO(&readSet);
+    FD_SET(STDIN_FILENO, &readSet);
+    struct timeval tv = {0, 25};
+    if (select(STDIN_FILENO+1, &readSet, NULL, NULL, &tv) < 0) perror("select");
+
+    if (!FD_ISSET(STDIN_FILENO, &readSet)) return -1;
+    unsigned char c;
+    c = getchar();
+
+    return c;
+}
+
+void print_motor(const motor_calibration& motor) {
+	cout << "ID: " << motor.id << " Addr: " << motor.addr << " Pos: "
+		<< motor.position << " Min:" << motor.min << " Max:" << motor.max << endl;
+}
+
+int update_state(ServoBus& bus, vector<motor_calibration>& motors) {
+
+	bus.update();
+
+	for (int i = 0; i < motors.size(); i++) {
+
+		if (motors[i].id > -1)
+			continue;
+
+		ServoHandler sv = bus.find(motors[i].addr);
+
+		motors[i].measurements.push_back(sv->getPosition());
+
+		if (motors[i].measurements.size() > 20) {
+
+			std::nth_element(motors[i].measurements.begin(), motors[i].measurements.begin() + motors[i].measurements.size()/2, motors[i].measurements.end());
+            int position = motors[i].measurements[motors[i].measurements.size()/2];
+
+			motors[i].min = std::min(motors[i].min, position);
+			motors[i].max = std::max(motors[i].max, position);
+
+			motors[i].delta += abs(motors[i].position - position);
+
+			motors[i].position = position;
+			motors[i].center = position;
+
+			print_motor(motors[i]);
+
+			motors[i].measurements.clear();
+
+		}
+	}
+
+}
+
+int main(int argc, char** argv) {
+
+	ServoBus bus;
+	struct termios old_tio, new_tio;
+	if (argc < 2 || !bus.open(argv[1])) {
+		cout << "Unable to connect to i2c bus" << endl;
+		return -1;
+	}
+
+	cout << "Welcome to OpenServo manipulator calibration utility" << endl;
+
+	int n = bus.scan();
+	cout << "Found " << n << " motors\n";
+
+	if (argc < 3) {
+
+		motors.resize(n);
+		for (int i = 0; i < n; i++) {
+			motors[i].min = 1000000;
+			motors[i].max = -1000000;
+			motors[i].center = 0;
+			motors[i].factor = 0;
+			motors[i].addr = bus.get(i)->getAddress();
+			motors[i].id = -1;
+		}
+
+
+	    /* get the terminal settings for stdin */
+	    tcgetattr(STDIN_FILENO,&old_tio);
+	    /* we want to keep the old setting to restore them a the end */
+	    new_tio = old_tio;
+	    /* disable canonical mode (buffered i/o) and local echo */
+	    new_tio.c_lflag &=(~ICANON & ~ECHO);
+	    /* set the new settings immediately */
+	    tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+
+		int m = 0;
+		while (m < n) {
+			cout << "Please move motor " << m << " to both extreme positions and then to center position and press any key." << endl;
+
+			while (wait_key() < 0) {
+				update_state(bus, motors);
+			}
+
+			int candidate = -1;
+			int max_delta = 0;
+
+			for (int i = 0; i < n; i++) {
+				int delta = motors[i].delta;
+				motors[i].delta = 0;
+
+				if (motors[i].id > -1)
+					continue;
+
+				if (delta > max_delta) {
+					max_delta = delta;
+					candidate = i;
+				}
+			}
+
+			motors[candidate].id = m;
+			m++;
+		}
+
+	    /* restore the former settings */
+	    tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+
+
+	} else {
+
+	  YAML::Node doc = YAML::LoadFile(argv[2]);
+
+      cout << "Loaded " << doc.size() << endl;
+	  for (int i = 0; i < doc.size(); i++) {
+	    motor_calibration d;
+
+		d.id = i;
+	    d.addr = doc[i]["id"].as<int>();
+	    d.min = doc[i]["min"].as<int>();
+	    d.max = doc[i]["max"].as<int>();
+	    d.center = doc[i]["center"].as<int>();
+	    d.factor = doc[i]["factor"].as<float>();
+
+	    motors.push_back(d);
+	  }
+
+	}
+
+	cout << "Writing data to motors" << endl;
+	for (int i = 0; i < motors.size(); i++) {
+
+		motors[i].max = min(motors[i].max, 4096 - 16);
+		motors[i].min = max(motors[i].min, 16);
+
+		cout << "Writing data to motor " << i << endl;
+
+		ServoHandler sv = bus.find(motors[i].addr);
+		sv->unlock();
+		sv->set("seek.max", motors[i].max);
+		sv->set("seek.min", motors[i].min);
+		bus.update();
+		sv->registersCommit();
+
+	}
+
+	cout << "Verifying ... " << endl;
+	bus.update(true);
+
+	for (int i = 0; i < motors.size(); i++) {
+
+		ServoHandler sv = bus.find(motors[i].addr);
+		cout << "Motor " << motors[i].addr << " min: " << sv->getMinSeek() << " max: " << sv->getMaxSeek() << endl;
+
+	}
+
+	std::sort(motors.begin(), motors.end(), compare);
+
+	YAML::Node sequence;  // starts out as null
+
+	for (int i = 0; i < n; i++) {
+		YAML::Node motor;
+		motor["id"] = motors[i].addr;
+		motor["min"] = motors[i].min;
+		motor["max"] = motors[i].max;
+		motor["center"] = motors[i].center;
+		motor["factor"] = hardcoded_factors[i];
+		sequence.push_back(motor);
+	}
+
+	cout << sequence << endl;
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,395 @@
+#include <cmath>
+#include <chrono>
+#include <numeric>
+#include <unistd.h>
+
+#include <openservo.h>
+
+#include <manus/files.h>
+#include <manus/manipulator.h>
+
+#include <yaml-cpp/yaml.h>
+
+#define MEDIAN_WINDOW 20
+
+using namespace openservo;
+
+typedef struct MotorData {
+	int servo_id;
+	int joint_id;
+	int AD_min;
+	int AD_max;
+	int AD_center;
+	float factor;
+} MotorData;
+
+class OpenServoManipulator : public Manipulator
+{
+public:
+	OpenServoManipulator(const string& device, const string& model, const string& servos);
+	~OpenServoManipulator();
+
+	virtual int size();
+	virtual bool move(int joint, float position, float speed);
+
+	virtual ManipulatorDescription describe();
+	virtual ManipulatorState state();
+
+	bool process();
+
+private:
+
+	virtual int load_description(const string& model, const string& servos);
+
+	struct ServoRuntimeData
+	{
+		int address;
+		std::deque<int> position_median;
+		std::deque<int> goal_median;
+	};
+
+  std::vector<ServoRuntimeData> runtime_data;
+
+  ManipulatorDescription _description;
+  ManipulatorState _state;
+  std::vector<MotorData> _servos;
+  int read_rate;
+
+  openservo::ServoBus bus;
+
+  int joint_to_motor(int joint);
+  int motor_to_joint(int motor);
+
+};
+
+
+/*
+    Joint description:
+      - min, max AD values -> dh_min in dh_max calculated based on parameters
+      - origin value for AD
+      - max angle
+      - conversion factor from AD to angle
+      - dh_theta, dh_alpha, dh_d, dh_a, dh_min (computed later), dh_max(computed later)
+*/
+
+
+float scale_servo_to_joint(const MotorData& si, float ad_pos) {
+
+  float pos_deg = (ad_pos - si.AD_center) * si.factor;
+  return round(pos_deg * 100.0) / 100.0;
+
+}
+
+float scale_joint_to_servo(const MotorData& si, float pos) {
+
+  float pos_ad = (pos / si.factor) + si.AD_center;
+  return pos_ad;
+
+}
+
+OpenServoManipulator::OpenServoManipulator(const string& device,
+    const string& model_file, const string& bind_file) {
+
+  read_rate = 30;
+  _state.state = MANIPULATORSTATETYPE_UNKNOWN;
+  _description.name = "i2c Robot Manipulator";
+  _description.version = 0.2f;
+
+  if (bus.open(device))
+    _state.state = MANIPULATORSTATETYPE_ACTIVE;
+
+  int num = bus.scan();
+
+  load_description(model_file, bind_file);
+
+  if (num < _servos.size())
+    throw ManipulatorException("Not enough servos detected.");
+
+}
+
+OpenServoManipulator::~OpenServoManipulator() {
+
+}
+
+bool parse_bindings(const string& filename, ManipulatorDescription& manipulator, vector<MotorData>& servos) {
+
+    YAML::Node doc = YAML::LoadFile(filename);
+/*
+    const YAML::Node& offset = doc["offset"];
+
+    manipulator.frame.origin.x = offset["origin"]["x"].as<float>();
+    manipulator.frame.origin.y = offset["origin"]["y"].as<float>();
+    manipulator.frame.origin.z = offset["origin"]["z"].as<float>();
+
+    manipulator.frame.rotation.x = offset["rotation"]["x"].as<float>();
+    manipulator.frame.rotation.y = offset["rotation"]["y"].as<float>();
+    manipulator.frame.rotation.z = offset["rotation"]["z"].as<float>();
+*/
+    const YAML::Node& joints = doc["motors"];
+
+    servos.clear();
+
+    for (int i = 0; i < joints.size(); i++) {
+
+        if (manipulator.joints.size() <= i) return false;
+
+        const YAML::Node& servo = joints[i];
+
+        if (servo.IsDefined()) {
+          MotorData d;
+
+          d.servo_id = servo["id"].as<int>();
+          d.joint_id = -1;
+          d.AD_min = servo["min"].as<int>();
+          d.AD_max = servo["max"].as<int>();
+          d.AD_center = servo["center"].as<int>();
+          d.factor = servo["factor"].as<float>();
+
+          servos.push_back(d);
+        }
+
+    }
+
+    return true;
+}
+
+
+int OpenServoManipulator::load_description(const string& model, const string& servos) {
+
+  string model_path = find_file(model);
+  string servos_path = find_file(servos);
+
+
+  if (!parse_description(model_path, _description)) {
+    throw ManipulatorException("Unable to parse manipulator model description");
+  }
+
+  if (!parse_bindings(servos_path, _description, _servos)) {
+    throw ManipulatorException("Unable to parse manipulator bindings description");
+  }
+
+  _state.joints.resize(_description.joints.size());
+  int j = 0;
+  for (int i = 0; i < _description.joints.size(); i++) {
+    _state.joints[i].type = JOINTSTATETYPE_IDLE;
+
+    if (_description.joints[i].type == JOINTTYPE_FIXED)
+      continue;
+
+    if (j >= _servos.size())
+      throw ManipulatorException("Not enough motors in calibration data.");
+
+    runtime_data.resize(runtime_data.size() + 1);
+    runtime_data[runtime_data.size() - 1].address = _servos[j].servo_id;
+    _servos[j].joint_id = i;
+    _description.joints[i].dh_min = scale_servo_to_joint(_servos[j], _servos[j].AD_min);
+    _description.joints[i].dh_max = scale_servo_to_joint(_servos[j], _servos[j].AD_max);
+
+    cout << "Verifying min-max data." << endl;
+    ServoHandler sv = bus.find(_servos[j].servo_id);
+
+    if (sv->getMinSeek() != _servos[j].AD_min || sv->getMaxSeek() != _servos[j].AD_max) {
+
+      cout << "Detected incorrect parameters, writing min-max data to motor " << i << endl;
+      sv->unlock();
+      sv->set("seek.max", _servos[j].AD_max);
+      sv->set("seek.min", _servos[j].AD_min);
+      bus.update();
+      sv->registersCommit();
+    }
+
+    j++;
+  }
+
+  cout << "Joints: " << _description.joints.size() << " Motors: " << runtime_data.size() << endl;
+
+  if (j != _servos.size())
+    throw ManipulatorException("Unassigned motors remaining.");
+
+  return 1;
+}
+
+int OpenServoManipulator::size() {
+
+  return _description.joints.size();
+
+}
+
+bool OpenServoManipulator::move(int joint, float position, float speed) {
+
+  int motor = joint_to_motor(joint);
+
+  if (motor < 0)
+    return false;
+
+  ServoHandler sv = bus.find(runtime_data[motor].address);
+
+  if (!sv)
+    return false;
+
+  float pos = ::round(scale_joint_to_servo(_servos[motor], position));
+
+  sv->setSeekPosition((int)pos);
+
+  runtime_data[motor].goal_median.clear();
+  runtime_data[motor].goal_median.assign(MEDIAN_WINDOW, (int)pos);
+
+  _state.joints[joint].goal = position;
+
+  return true;
+
+}
+
+#define MEDIAN_WINDOW 20
+
+ManipulatorDescription OpenServoManipulator::describe() {
+
+  return _description;
+
+}
+
+ManipulatorState OpenServoManipulator::state() {
+
+  return _state;
+
+}
+
+int OpenServoManipulator::joint_to_motor(int j) {
+
+  if (j < 0 || j >= _description.joints.size()) return -1;
+
+  if (_description.joints[j].type == JOINTTYPE_FIXED) return -1;
+
+  int m = 0;
+  for (int i = 0; i < j; i++) {
+    if (_description.joints[i].type != JOINTTYPE_FIXED)
+      m++;
+  }
+
+  return m;
+
+}
+
+int OpenServoManipulator::motor_to_joint(int m) {
+
+  int mt = m;
+  int j = 0;
+  while (m > 0) {
+    j++; m--;
+    if (_description.joints[j].type == JOINTTYPE_FIXED)
+      j++;
+  }
+
+  return j;
+
+}
+
+bool OpenServoManipulator::process() {
+
+  if (!bus.update()) {
+    cout << bus.getLastError() << endl;
+    //return false;
+  }
+
+  // refresh state data
+  for (int q = 0; q < _description.joints.size(); q++) {
+
+    int motor = joint_to_motor(q);
+
+    if (motor < 0) continue;
+
+    ServoHandler sv = bus.find(runtime_data[motor].address);
+
+    if (!sv) continue;
+
+    runtime_data[motor].position_median.push_back(sv->getPosition());
+    runtime_data[motor].goal_median.push_back(sv->getSeekPosition());
+
+    if (runtime_data[motor].position_median.size() > MEDIAN_WINDOW)
+      runtime_data[motor].position_median.pop_front();
+
+    if (runtime_data[motor].goal_median.size() > MEDIAN_WINDOW)
+      runtime_data[motor].goal_median.pop_front();
+
+    vector<int> sorted_position(runtime_data[motor].position_median.begin(),
+      runtime_data[motor].position_median.end());
+    vector<int> sorted_goal(runtime_data[motor].goal_median.begin(),
+      runtime_data[motor].goal_median.end());
+
+    std::nth_element(sorted_position.begin(),
+      sorted_position.begin() + sorted_position.size() / 2, sorted_position.end());
+
+    float position = sorted_position[sorted_position.size() / 2];
+
+    std::nth_element(sorted_goal.begin(),
+      sorted_goal.begin() + sorted_goal.size() / 2, sorted_goal.end());
+    float goal = sorted_goal[sorted_goal.size() / 2];
+
+    _state.joints[q].position = scale_servo_to_joint(_servos[motor], position);
+    _state.joints[q].goal = scale_servo_to_joint(_servos[motor], goal);
+    _state.joints[q].speed = 1;
+
+  }
+
+  _state.header.timestamp = std::chrono::system_clock::now();
+
+  return true;
+
+}
+
+#define REFRESH_DELTA 20
+
+using namespace std::chrono;
+
+int main(int argc, char** argv) {
+
+  string model(get_env("MANUS_MANIPULATOR_MODEL", "manipulator.yaml"));
+  string servos(get_env("MANUS_MANIPULATOR_SERVOS", "servos.yaml"));
+  string device;
+
+  if (argc > 1) {
+    device = string(argv[1]);
+  }  
+
+  if (argc > 2) {
+    model = string(argv[2]);
+  } 
+
+  if (argc > 3) {
+    servos = string(argv[3]);
+  }
+
+  cout << "Starting OpenServo manipulator" << endl;
+
+  try {
+
+    shared_ptr<OpenServoManipulator> manipulator =
+      shared_ptr<OpenServoManipulator>(new OpenServoManipulator(
+                                         device, model, servos));
+
+    SharedClient client = echolib::connect(string(), "manipulator");
+    ManipulatorManager manager(client, manipulator);
+
+    int duration = 0;
+
+    while (true) {
+      if (!echolib::wait(std::max(1, 20 - duration))) break;
+
+      steady_clock::time_point start = steady_clock::now();
+
+      manager.update();
+      if (!manipulator->process()) break;
+
+      duration = duration_cast<milliseconds>(steady_clock::now() - start).count();
+
+    }
+
+  } catch (ManipulatorException &e) {
+     cout << "Exception: " << e.what() << endl;
+
+     exit(1);
+  }
+
+
+  exit(0);
+}
